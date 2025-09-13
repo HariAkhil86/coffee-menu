e690e071775bed2b0033f9fab52a5359ce2c4532
HariAkhil86 <hariakhilkothuri@gmail.com>
2025-09-13 15:44:56 +0530

    fix: mark AppComponent as standalone and correct styleUrls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CoffeeListComponent }  from './components/coffee-list/coffee-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [CoffeeListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'coffee-menu';
