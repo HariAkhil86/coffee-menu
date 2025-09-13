7a6621fc65387039c4ce2c34834d65f4fcd17e73
HariAkhil86 <hariakhilkothuri@gmail.com>
2025-09-13 15:55:40 +0530

    feat: implement CoffeeService with API call and Coffee interface

--- src/app/services/coffee.service.ts
+++ src/app/services/coffee.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Coffee {
+  id: number;
+  title: string;
+  description: string;
+  ingredients: string[];
+  image: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoffeeService {
+  private apiUrl = 'https://api.sampleapis.com/coffee/hot';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCoffees(): Observable<Coffee[]> {
+    return this.http.get<Coffee[]>(this.apiUrl);
+  }
 }
